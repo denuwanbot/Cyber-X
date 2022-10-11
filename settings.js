e954c97174628e52f5fc8ee07279034943cc83f3
denuwanbot <113087692+denuwanbot@users.noreply.github.com>
2022-10-11 05:07:53 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,23 +43,23 @@ global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.owner = ['94776727209']
+global.ownertag = '94776727209' //ur tag number
+global.botnumber = '94776727209' //Add your Bot Number For Log
+global.botname = 'DENUWAN-BOT' //ur bot name
+global.ig = "https://www.youtube.com/channel/UCP25W-WEcWBGWXuEZFh3SWA" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.youtube.com/channel/UCP25W-WEcWBGWXuEZFh3SWA" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.linkz = 'https://www.youtube.com/channel/UCP25W-WEcWBGWXuEZFh3SWA'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
-global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
-global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
-global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
+global.packname = "DENUWAN BOT" //ur sticker watermark packname
+global.author = "DENUWAN" //ur sticker watermark author
+global.footer = "DENUWAN BOT"
+global.aliveimg ="https://ibb.co/mJf6v29"
+global.allmenuimg ="https://ibb.co/mJf6v29"
+global.welcomeimg= "https://ibb.co/F5kvyB9"
+global.gbyeimg = "https://ibb.co/SfmzXp3"
 global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
 
 //BOT MEDIAS
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94776727209'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
